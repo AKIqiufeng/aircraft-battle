7488a274011656f98dd723c2e32b3a325a001fbe
mioling <982060608@qq.com>
2024-11-19 14:40:46 +0800

    添加死亡动画

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
+import functools
+import sys
 from random import randint
 import pygame as pg
 from pygame.locals import *
 import os
-
 
 WIDTH = 480
 HEIGHT = 700
@@ -13,6 +14,7 @@ ENEMYRATES = 1000
 BULLETRATES = 200
 
 
+@functools.lru_cache()
 def load_animation(imageName):  # 加载帧序列图片
     images = []
     count = 0
@@ -40,21 +42,59 @@ class Player(pg.sprite.Sprite):  # 玩家类
         self.image = self.images[0]
         self.rect = self.image.get_rect(midbottom=(WIDTH / 2, HEIGHT))
         self.mask = pg.mask.from_surface(self.image)
-        self.downImages = load_animation("./resource/image/me_destroy_{}.png")
-        self.downIdx = 0
 
-        self.lifeNum = 3
+        # 加载死亡动画素材
+        self.deathImages = load_animation("./resource/image/me_destroy_{}.png")
+        self.deathIdx = 0  # 用于跟踪死亡动画的索引
+        self.invincible_end_time = 0
+        self.death_animation_timer = 0
+
+        # 加载生命素材
+        self.life_image = pg.image.load("./resource/image/life.png").convert_alpha()
+        self.life_rect = self.life_image.get_rect()
+        self.lifeNum = 3  # 默认生命数量
+
+    def draw_life(self, screen):
+        # 根据生命数绘制生命图标
+        for i in range(self.lifeNum):
+            x = 10 + i * (self.life_rect.width + 10)  # 计算每个生命图标的x坐标
+            y = 10  # 生命图标的y坐标，可以根据需要调整
+            screen.blit(self.life_image, (x, y))  # 在屏幕上绘制生命图标
 
     def update(self):  # 更新玩家图片，实现动画效果
-        for enemy1 in enemy_group:  # 遍历敌机组（也可以写成enemy_group.sprites()）
-            if collide_mask(self.mask, enemy1.mask, self.rect, enemy1.rect):  # 判断玩家是否与敌机发生碰撞
-                enemy1.kill()
-                self.lifeNum -= 1
-                if self.lifeNum == 0:
-                    pg.quit()
+        # 播放死亡动画和无敌状态
+        if self.invincible_end_time == 0:
+            for enemy1 in enemy_group:  # 遍历敌机组（也可以写成enemy_group.sprites()）
+                if collide_mask(self.mask, enemy1.mask, self.rect, enemy1.rect):  # 判断玩家是否与敌机发生碰撞
+                    enemy1.kill()
+                    self.lifeNum -= 1  # 扣除生命
+                    if self.lifeNum == 0:
+                        pg.quit()
+                    else:
+                        # 设置无敌时间和死亡动画
+                        self.invincible_end_time = pg.time.get_ticks() + 3000  # 3秒
+                        self.deathIdx = 0  # 重置死亡动画索引
+                        self.death_animation_timer = pg.time.get_ticks()  # 重置死亡动画计时器
+
+        # 播放死亡动画
+        if self.invincible_end_time > pg.time.get_ticks():
+            current_time = pg.time.get_ticks()
+            if current_time - self.death_animation_timer > 100:  # 检查是否已过100ms
+                if self.deathIdx < len(self.deathImages):
+                    self.image = self.deathImages[self.deathIdx]
+                    self.deathIdx = self.deathIdx + 1
+                    self.death_animation_timer = current_time  # 重置计时器
+                else:
+                    self.image = self.images[self.imageIdx % len(self.images)]
+                    self.image.set_alpha(randint(0, 255))
+
+        if pg.time.get_ticks() >= self.invincible_end_time:
+            self.deathIdx = 0
+            self.invincible_end_time = 0
+            self.image.set_alpha(255)
 
         nowtime = pg.time.get_ticks()
-        if nowtime - self.lastAniTime > 100:
+        if nowtime - self.lastAniTime > 100 and self.deathIdx >= len(self.deathImages):
             self.lastAniTime = nowtime
             self.imageIdx += 1
             self.image = self.images[self.imageIdx % len(self.images)]
@@ -84,7 +124,7 @@ class Enemy1(pg.sprite.Sprite):  # 敌人1类
         super().__init__(*group)
 
         self.image = pg.image.load("./resource/image/enemy1.png")
-        self.rect = self.image.get_rect(bottom=0, centerx=randint(29, WIDTH-29))
+        self.rect = self.image.get_rect(bottom=0, centerx=randint(29, WIDTH - 29))
         self.mask = pg.mask.from_surface(self.image)
         self.lastAniTime = 0
         self.downImages = load_animation("./resource/image/enemy1_down{}.png")
@@ -130,7 +170,6 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))  # 设置窗口大小
 clock = pg.time.Clock()  # 设置时钟，用以帧数控制
 pg.display.set_caption("飞机大战")  # 窗口标题
 
-
 pg.mixer.music.load("./resource/sound/game_music.ogg")  # 设置背景音乐
 pg.mixer.music.play(-1)
 pg.mixer.music.set_volume(0.5)
@@ -154,7 +193,7 @@ while True:
 
     screen.blit(bg, bgRect)  # 显示背景图片
     all_group.draw(screen)  # 绘制所有对象
-
+    player.draw_life(screen)  # 绘制玩家的生命数
     all_group.update()
 
     col = pg.sprite.groupcollide(bullet_group, enemy_group, True, False)
@@ -175,7 +214,7 @@ while True:
     for event in pg.event.get():  # 读取事件列表
         if event.type == QUIT:  # 退出事件
             pg.quit()
-            exit()
+            sys.exit()
 
     clock.tick(144)  # 最大帧率设置为144
     pg.display.flip()  # 刷新绘制内容
